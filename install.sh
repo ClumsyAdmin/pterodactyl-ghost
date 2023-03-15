53dbd7c94f96da0ed640d9a66fc8416eb65f8b2b
Clumsy <59402569+ClumsyAdmin@users.noreply.github.com>
2023-03-14 21:04:52 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,18 @@ chmod -R 755 /.npm
 
 cp -r ./temp/caddy /mnt/server/
 cp ./temp/start.sh /mnt/server
-curl "https://caddyserver.com/api/download?os=linux&arch=amd64&idempotency=33572405766393" -s --output /mnt/server/caddy-server
+# Get the latest release tag
+TAG=$(curl -s https://api.github.com/repos/caddyserver/caddy/releases/latest | grep tag_name | cut -d '"' -f 4)
+# Get the download URL for the latest release
+URL=$(curl -s https://api.github.com/repos/caddyserver/caddy/releases/latest | grep browser_download_url | grep linux_amd64.tar.gz | cut -d '"' -f 4)
+# Download the binary using curl and output it to the specified file
+curl -LJO# -s --output /mnt/server/caddy-server $URL
+filename=$(basename $URL)
+mv /mnt/server/caddy/ /mnt/server/caddytemp/
+tar -xf $filename
+rm /mnt/server/caddy-server
+mv /mnt/server/caddy /mnt/server/caddy-server
+mv /mnt/server/caddytemp/ /mnt/server/caddy/
 
 chmod +x /mnt/server/caddy-server
 chmod +x /mnt/server/start.sh
